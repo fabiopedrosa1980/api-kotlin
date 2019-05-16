b8b3cb2913ede88c882e1b6449391e1d7f26ebff
Fabio Henrique Sampaio Pedrosa <fpedrosa@laptec.com.br>
2019-05-16 14:50:23 -0300

    adding other methods

--- src/main/kotlin/br/com/pedrosa/apikotlin/controller/ClientController.kt
+++ src/main/kotlin/br/com/pedrosa/apikotlin/controller/ClientController.kt
@@ -2,32 +2,40 @@ package br.com.pedrosa.apikotlin.controller
 
 import br.com.pedrosa.apikotlin.repository.ClientRepository
 import br.com.pedrosa.apikotlin.domain.Client
+import org.springframework.web.bind.annotation.*
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.beans.factory.annotation.Autowired
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 
 @RestController
 @RequestMapping("client")
-class ClientController {
+class ClientController(private val clientRepository: ClientRepository) {
 
-    @Autowired
-    lateinit var clientRepository: ClientRepository
 
     @GetMapping
     fun list(): Flux<Client> {
         return this.clientRepository.findAll()
     }
 
+    @GetMapping("{id}")
+    fun find(@PathVariable id: String): Mono<Client> {
+        return this.clientRepository.findById(id)
+    }
+
     @PostMapping
     fun add(@RequestBody client: Client): Mono<Client> {
         return this.clientRepository.save(client)
+    }
+
+    @PutMapping
+    fun update(@RequestBody client: Client): Mono<Client> {
+        return this.clientRepository.save(client)
+    }
+
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: String) {
+        this.clientRepository.deleteById(id)
     }
 
 }
